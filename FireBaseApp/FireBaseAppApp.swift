42392159c2beadd3e562f265f4de68152b3d010a
Abraham May <Abrahammay1015@gmail.com>
2024-07-25 12:31:10 -0500

    Initial Commit

---
+++ FireBaseApp/FireBaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireBaseAppApp.swift
+//  FireBaseApp
+//
+//  Created by Abraham May on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct FireBaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
