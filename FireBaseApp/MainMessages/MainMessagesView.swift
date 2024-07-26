362fbfff99888ab545fd0acc71c333643bc26c28
Abraham May <Abrahammay1015@gmail.com>
2024-07-26 18:07:35 -0500

    Was able to set up firestore to see a collection of users

---
+++ FireBaseApp/MainMessages/MainMessagesView.swift
@@ -0,0 +1,18 @@
+//
+//  MainMessagesView.swift
+//  FireBaseApp
+//
+//  Created by Abraham May on 7/26/24.
+//
+
+import SwiftUI
+
+struct MainMessagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainMessagesView()
+}
